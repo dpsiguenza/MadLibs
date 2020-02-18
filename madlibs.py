8b3f84786a983eee8b74d6c915017088d01e97cb
dpsiguenza <dpsiguenza@gmail.com>
2020-02-18 13:50:32 +0000

    Created an empty list and printed each item in that list. Ctrl+K and Ctrl+C to comment. Ctrl+k and Ctrl+U to uncomment

--- madlibs.py
+++ madlibs.py
@@ -1,12 +1,20 @@
-def getWord(wordDescription):
-    wordType = input(f"Please give me a {wordDescription} ")
-    return wordType
+# def getWord(wordDescription):
+#     wordType = input(f"Please give me a {wordDescription} ")
+#     return wordType
 
-sillyword = getWord('silly word')
-pluralnoun = getWord('plural noun')
-bodypartplural = getWord('part of the body (plural)')
-noun = getWord('noun')
+# sillyword = getWord('silly word')
+# pluralnoun = getWord('plural noun')
+# bodypartplural = getWord('part of the body (plural)')
+# noun = getWord('noun')
 
-madLib = (f"'{sillyword}! Playing Mad Libs is way more fun than squishing {pluralnoun} with my bare {bodypartplural}. I don't even have to take a hot {noun} afterward!'")
-print(madLib)
+# madLib = (f"'{sillyword}! Playing Mad Libs is way more fun than squishing {pluralnoun} with my bare {bodypartplural}. I don't even have to take a hot {noun} afterward!'")
+# print(madLib)
 
+### Below is how to create an empty list, add things to the list by input and loop through each item in the list and print it
+### Next I want to create an empty list, ask for 5 nouns, put them in the list and call them each, then add them to the mad lib to print
+# list =[]
+# list.append(input("Give me something: "))
+# list.append(input("Give me something else: "))
+
+# for i in range(len(list)):
+#     print(list[i])
