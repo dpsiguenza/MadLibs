5743c0e1ba0adaa54ea3977948da244f11ed97f1
dpsiguenza <dpsiguenza@gmail.com>
2020-02-18 12:15:56 +0000

    Initial code

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,12 @@
-print("Hello World")
+def getWord(wordDescription):
+    wordType = input(f"Please give me a {wordDescription} ")
+    return wordType
+
+sillyword = getWord('silly word')
+pluralnoun = getWord('plural noun')
+bodypartplural = getWord('part of the body (plural)')
+noun = getWord('noun')
+
+madLib = (f"'{sillyword}! Playing Mad Libs is way more fun than squishing {pluralnoun} with my bare {bodypartplural}. I don't even have to take a hot {noun} afterward!'")
+print(madLib)
+
